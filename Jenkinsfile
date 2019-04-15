9c27582662e0f088d30bb4876c220848a2da9b41
Chris Williams <chris.a.williams@gmail.com>
2019-04-15 14:36:32 -0400

    build(jenkins): link to generated artifacts

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,4 @@
-library 'pipeline-library'
+library 'pipeline-library@module-artifacts'
 
 buildModule {
 	sdkVersion = '7.5.2.GA'
--- dangerfile.js
+++ dangerfile.js
@@ -4,12 +4,24 @@
 const junit = require('@seadub/danger-plugin-junit').default;
 const dependencies = require('@seadub/danger-plugin-dependencies').default;
 
+// Add links to artifacts we've stuffed into the ENV.ARTIFACTS variable
+async function linkToArtifacts() {
+	if (ENV.BUILD_STATUS === 'SUCCESS' || ENV.BUILD_STATUS === 'UNSTABLE') {
+		const artifacts = ENV.ARTIFACTS.split(';');
+		if (artifacts.length !== 0) {
+			const artifactsListing = '- ' + artifacts.map(a => danger.utils.href(`${ENV.BUILD_URL}artifact/${a}`, a)).join('\n- ');
+			message(`:floppy_disk: Here are the artifacts produced:\n${artifactsListing}`);
+		}
+	}
+}
+
 async function main() {
 	// do a bunch of things in parallel
 	// Specifically, anything that collects what labels to add or remove has to be done first before...
 	await Promise.all([
 		junit({ pathToReport: './TESTS-*.xml' }),
 		dependencies({ type: 'npm' }),
+		linkToArtifacts(),
 	]);
 }
 main()
