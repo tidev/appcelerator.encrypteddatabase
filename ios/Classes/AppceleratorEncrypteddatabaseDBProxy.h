55bab8339620807cec53e3afcd1b7ee0c6d30b43
Chris Williams <chris.a.williams@gmail.com>
2019-10-15 15:43:43 -0400

    style(ios): fix formatting

--- ios/Classes/AppceleratorEncrypteddatabaseDBProxy.h
+++ ios/Classes/AppceleratorEncrypteddatabaseDBProxy.h
@@ -3,34 +3,34 @@
  * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
  * Proprietary and Confidential - This source code is not for redistribution
  */
- 
+
+#import "EncPlausibleDatabase.h"
 #import "TiProxy.h"
 #import <sqlite3.h>
-#import "EncPlausibleDatabase.h"
 
 @interface AppceleratorEncrypteddatabaseDBProxy : TiProxy {
-@protected
-	NSString *name;
-	EncPLSqliteDatabase *database;
-	NSMutableArray *statements;
+  @protected
+  NSString *name;
+  EncPLSqliteDatabase *database;
+  NSMutableArray *statements;
 }
 
-@property(nonatomic,readonly) NSString *name;
-@property(nonatomic,readonly) NSNumber *rowsAffected;
-@property(nonatomic,readonly) NSNumber *lastInsertRowId;
-@property(nonatomic,readwrite,retain) NSString* password;
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) NSNumber *rowsAffected;
+@property (nonatomic, readonly) NSNumber *lastInsertRowId;
+@property (nonatomic, readwrite, retain) NSString *password;
 
--(NSDictionary*)cipherUpgrade:(NSString*)name_;
--(NSNumber*)isCipherUpgradeRequired:(id)args;
--(void)open:(NSString*)name;
--(void)install:(NSString*)path name:(NSString*)name;
--(id)execute:(id)args;
--(void)close:(id)args;
--(void)remove:(id)args;
+- (NSDictionary *)cipherUpgrade:(NSString *)name_;
+- (NSNumber *)isCipherUpgradeRequired:(id)args;
+- (void)open:(NSString *)name;
+- (void)install:(NSString *)path name:(NSString *)name;
+- (id)execute:(id)args;
+- (void)close:(id)args;
+- (void)remove:(id)args;
 
 #pragma mark Internal
 
--(void)removeStatement:(EncPLSqliteResultSet*)statement;
--(EncPLSqliteDatabase*)database;
+- (void)removeStatement:(EncPLSqliteResultSet *)statement;
+- (EncPLSqliteDatabase *)database;
 
 @end
--- ios/Classes/AppceleratorEncrypteddatabaseModule.h
+++ ios/Classes/AppceleratorEncrypteddatabaseModule.h
@@ -3,23 +3,22 @@
  * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
  * Proprietary and Confidential - This source code is not for redistribution
  */
- 
-#import "TiModule.h"
+
 #import "AppceleratorEncrypteddatabaseDBProxy.h"
+#import "TiModule.h"
 
 typedef enum {
-	TiFieldTypeUnknown = -1,
-	TiFieldTypeString,
-	TiFieldTypeInt,
-	TiFieldTypeFloat,
-	TiFieldTypeDouble
+  TiFieldTypeUnknown = -1,
+  TiFieldTypeString,
+  TiFieldTypeInt,
+  TiFieldTypeFloat,
+  TiFieldTypeDouble
 } AppceleratorDatabaseFieldType;
 
-@interface AppceleratorEncrypteddatabaseModule : TiModule 
-{
+@interface AppceleratorEncrypteddatabaseModule : TiModule {
 }
 
-@property(nonatomic,readwrite,retain) NSString* password;
--(id)cipherUpgrade:(id)path;
--(NSNumber*)isCipherUpgradeRequired:(id)args;
+@property (nonatomic, readwrite, retain) NSString *password;
+- (id)cipherUpgrade:(id)path;
+- (NSNumber *)isCipherUpgradeRequired:(id)args;
 @end
