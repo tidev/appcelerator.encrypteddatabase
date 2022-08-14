5e82b4f8550e236c536f6a5ccc8a6407eb4a5c0b
m1ga <migamiga@web.de>
2022-08-14 12:01:48 +0200

    update

--- example/JSON1-Extension/app_json1.js
+++ example/JSON1-Extension/app_json1.js
@@ -9,14 +9,14 @@ win.add(btn);
 win.open();
 
 function accessDatabase() {
-	dbobj.setPassword('secret');
-	
+	dbobj.password = 'secret';
+
 	Ti.API.info('Opening DB ...');
 	const instance = dbobj.open('test.db');
-	
+
 	instance.execute('CREATE TABLE IF NOT EXISTS user(name string, phone string);');
 	instance.execute('insert into user (name, phone) values("oz", json(\'{"cell":"+491765", "home":"+498973"}\'));');
-	
+
 	const dataToInsertHandle = instance.execute('select user.phone from user where user.name==\'oz\'');
 	const result = dataToInsertHandle.isValidRow() ? dataToInsertHandle.fieldByName('phone') : null;
 
